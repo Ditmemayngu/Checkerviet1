bce5d10ad4ce12f138f5639c5d5fa34b550e7050
Ditmemayngu <dumaxaigoi@gmail.com>
2026-06-22 04:14:37 +0700

    Create api/config.js for Scamalytics configuration
    Add configuration file for Scamalytics with user and API key.

---
+++ api/config.js
@@ -0,0 +1,20 @@
+// ================================
+// SCAMALYTICS CONFIG
+// ================================
+// Dán user và API key của bạn vào đây.
+// Không cần dùng Environment Variables trên Vercel nữa.
+//
+// LƯU Ý QUAN TRỌNG:
+// Nếu upload code này lên GitHub PUBLIC thì API key sẽ bị lộ.
+// Hãy để GitHub repo là PRIVATE.
+
+module.exports = {
+  SCAMALYTICS_USER: "6a37a1d28d5f2",
+  SCAMALYTICS_KEY: "7adc0475d44cace9252f751250b9b9096bbe6de3486212df577fcba1c6538d97",
+
+  // US node:
+  SCAMALYTICS_HOST: "https://api11.scamalytics.com/v3",
+
+  // Nếu account của bạn là EU node thì đổi dòng trên thành:
+  // SCAMALYTICS_HOST: "https://api12.scamalytics.com/v3",
+};
